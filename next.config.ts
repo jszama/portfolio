52f7aec4de58cfe949b7d9ba3de339ae13b3c7a7
jszama <jmszamotulski@gmail.com>
2025-05-25 11:34:20 +0100

    config change

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,9 @@ const nextConfig: NextConfig = {
   },
   images: {
     disableStaticImages: false,
+    qualities: [50, 60, 80, 90],
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
     remotePatterns: [
       {
         protocol: "https",
