8072dcaea34918d6d7e63bb868cd5b45a85954f6
jszama <jmszamotulski@gmail.com>
2025-08-07 17:29:09 +0100

    add current FAQ section implementation

---
+++ src/app/__components/FAQ/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function FAQ() {
+	return (
+		<section
+			id="testimonials"
+			className="flex flex-col px-4 py-12 sm:mt-24 sm:mx-24 sm:py-0 sm:pb-12 sm:px-0 relative"
+		>
+			<h2 className="z-10 text-3xl text-[var(--primary-text)] font-bold">FAQ</h2>
+			<h3 className="z-10 text-md text-[var(--secondary-text)] mb-4">
+				A little about what is going on...
+			</h3>
+		</section>
+	);
+}
