7d4f1a132562c8637eccb477ecabf6255980f67f
jszama <jmszamotulski@gmail.com>
2025-08-07 17:18:23 +0100

    update hero section and related components

--- src/app/__components/VideoModalButton.tsx
+++ src/app/__components/VideoModalButton.tsx
@@ -3,31 +3,35 @@ import { useState } from "react";
 import Modal from "./Modal";
 
 interface VideoModalButtonProps {
-  text: string;
-  videoId: string;
+	text: string;
+	videoId: string;
 }
 
 export default function VideoModalButton({ text, videoId }: VideoModalButtonProps) {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <>
-      <button
-        className="transition-all duration-200 hover:scale-105 bg-[var(--primary-magenta)] hover:bg-[var(--hover-magenta)] text-white font-bold py-2 px-4 rounded cursor-pointer w-full h-auto sm:w-auto"
-        type="button"
-        onClick={() => setIsOpen(true)}
-      >
+	return (
+		<>
+			<button
+				className="transition-all duration-200 hover:scale-105 bg-[var(--primary-magenta)] hover:bg-[var(--hover-magenta)] text-white font-bold py-2 px-4 rounded cursor-pointer w-full h-auto sm:w-auto"
+				type="button"
+				onClick={() => setIsOpen(true)}
+			>
 				{text}
 			</button>
-      <Modal className="bg-black rounded-2xl w-full aspect-video" isOpen={isOpen} setIsOpen={setIsOpen}>
-        <iframe
-          className="z-0 rounded-2xl w-full h-full"
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-          title="YouTube video about me"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share"
-          allowFullScreen={true}
-        />
-      </Modal>
-    </>
-  );
+			<Modal
+				className="bg-black rounded-2xl w-full aspect-video"
+				isOpen={isOpen}
+				setIsOpen={setIsOpen}
+			>
+				<iframe
+					className="z-0 rounded-2xl w-full h-full"
+					src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
+					title="YouTube video about me"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share"
+					allowFullScreen={true}
+				/>
+			</Modal>
+		</>
+	);
 }
--- src/app/__components/hero/assets/TechStackCarousel.tsx
+++ src/app/__components/hero/assets/TechStackCarousel.tsx
@@ -1,59 +1,82 @@
-'use client'
-import type React from 'react'
-import type { EmblaOptionsType, EmblaPluginType } from 'embla-carousel'
-import Autoplay from 'embla-carousel-autoplay'
-import useEmblaCarousel from 'embla-carousel-react'
-import Image from 'next/image'
+"use client";
+import type { EmblaOptionsType, EmblaPluginType } from "embla-carousel";
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
+import { type CSSProperties, type FC } from "react";
 
-import { MongoDBIcon, NextJsIcon, JavaIcon, PythonIcon, JavascriptIcon, ReactIcon, TypescriptIcon } from '../../TechStackIcons'
+import {
+	JavaIcon,
+	JavascriptIcon,
+	MongoDBIcon,
+	NextJsIcon,
+	PythonIcon,
+	ReactIcon,
+	TypescriptIcon,
+} from "../../TechStackIcons";
 
 const slides = [
-  { icon: MongoDBIcon, tooltip: '1 year of experience' },
-  { icon: NextJsIcon, tooltip: '1 year of experience' },
-  { icon: ReactIcon, tooltip: '2 years of experience' },
-  { icon: TypescriptIcon, tooltip: '2 years of experience' },
-  { icon: JavaIcon, tooltip: '1 year of experience' },
-  { icon: PythonIcon, tooltip: '2 years of experience' },
-  { icon: JavascriptIcon, tooltip: '2 years of experience' },
-]
+	{ icon: MongoDBIcon, tooltip: "1 year of experience" },
+	{ icon: NextJsIcon, tooltip: "1 year of experience" },
+	{ icon: ReactIcon, tooltip: "2 years of experience" },
+	{ icon: TypescriptIcon, tooltip: "2 years of experience" },
+	{ icon: JavaIcon, tooltip: "1 year of experience" },
+	{ icon: PythonIcon, tooltip: "2 years of experience" },
+	{ icon: JavascriptIcon, tooltip: "2 years of experience" },
+];
 
 type PropType = {
-  options?: EmblaOptionsType
-  size: number
-}
+	options?: EmblaOptionsType;
+	size: number;
+};
 
-const TechStackCarousel: React.FC<PropType> = (props) => {
-  const { options, size } = props
-  const [emblaRef] = useEmblaCarousel(options, [
-    Autoplay({ delay: 1000, stopOnInteraction: false, stopOnMouseEnter: true }) as EmblaPluginType,
-  ])
+const TechStackCarousel: FC<PropType> = (props) => {
+	const { options, size } = props;
+	const [emblaRef] = useEmblaCarousel(options, [
+		Autoplay({
+			delay: 1000,
+			stopOnInteraction: false,
+			stopOnMouseEnter: true,
+		}) as EmblaPluginType,
+	]);
 
-  return (
-    <section
-      className="rounded-sm max-w-[8rem]"
-      style={{
-        '--slide-height': '2rem',
-        '--slide-spacing': '1rem',
-        '--slide-size': '25%',
-      } as React.CSSProperties}
-    >
-      <div className="overflow-hidden h-auto p-1" ref={emblaRef}>
-        <div className="techstack-embla__container flex touch-pinch-zoom touch-pan-y ml-[var(--slide-spacing) * -1]">
-          {slides.map((value) => (
-            <div
-              className="ml-[var(--slide-spacing)] min-w-0"
-              key={value.icon}
-              style={{ transform: 'translate3d(0, 0, 0)', flex: '0 0 var(--slide-size)' }}
-            >
-                <div className="rounded-[1.8rem] flex items-center justify-center select-none h-[var(--slide-height)] group relative">
-                  <Image title={value.tooltip} src={value.icon.toString()} width={size} height={size} alt="tech" />
-                </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  )
-}
+	return (
+		<section
+			className="rounded-sm max-w-[8rem]"
+			style={
+				{
+					"--slide-height": "2rem",
+					"--slide-spacing": "1rem",
+					"--slide-size": "25%",
+				} as CSSProperties
+			}
+		>
+			<div className="overflow-hidden h-auto p-1" ref={emblaRef}>
+				<div className="techstack-embla__container flex touch-pinch-zoom touch-pan-y ml-[var(--slide-spacing) * -1]">
+					{slides.map((value) => (
+						<div
+							className="ml-[var(--slide-spacing)] min-w-0"
+							key={value.icon}
+							style={{
+								transform: "translate3d(0, 0, 0)",
+								flex: "0 0 var(--slide-size)",
+							}}
+						>
+							<div className="rounded-[1.8rem] flex items-center justify-center select-none h-[var(--slide-height)] group relative">
+								<Image
+									title={value.tooltip}
+									src={value.icon.toString()}
+									width={size}
+									height={size}
+									alt="Programming Language Icon"
+								/>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</section>
+	);
+};
 
-export default TechStackCarousel
+export default TechStackCarousel;
