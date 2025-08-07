fb012b098160613a2a3e7f1477850c50e785ca8d
jszama <jmszamotulski@gmail.com>
2025-08-07 17:24:33 +0100

    optimise slide in animation

--- src/app/__components/SlideInAnimation.tsx
+++ src/app/__components/SlideInAnimation.tsx
@@ -1,26 +1,45 @@
-import React from 'react'
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
+import { useMemo } from "react";
 
 type SlideInAnimationProps = {
-    duration?: number;
-    from?: 'left' | 'right' | 'top' | 'bottom';
-    delay?: number;
-    children: React.ReactNode;
+	duration?: number;
+	from?: "left" | "right" | "top" | "bottom";
+	delay?: number;
+	children: React.ReactNode;
 };
 
-export default function SlideInAnimation({ duration = 0.5, from, delay, children }: SlideInAnimationProps) {
-    return (
-        <motion.div
-            initial={{
-                x: from === 'left' ? -150 : from === 'right' ? 150 : 0,
-                y: from === 'top' ? -150 : from === 'bottom' ? 150 : 0,
-                opacity: 0
-            }}
-            whileInView={{ x: 0, y: 0, opacity: 1 }}
-            transition={{ duration, delay, ease: [0.25, 1.0, 0.25, 1.0]}}
-            viewport={{ once: true, amount: 0.3 }}
-        >
-            {children}
-        </motion.div>
-    );
+export default function SlideInAnimation({
+	duration = 0.5,
+	from = "left",
+	delay = 0,
+	children,
+}: SlideInAnimationProps) {
+	const initial = useMemo(
+		() => ({
+			x: from === "left" ? -150 : from === "right" ? 150 : 0,
+			y: from === "top" ? -150 : from === "bottom" ? 150 : 0,
+			opacity: 0,
+		}),
+		[from]
+	);
+
+	const transition = useMemo(
+		() => ({
+			duration,
+			delay,
+			ease: [0.25, 0.1, 0.25, 1.0],
+		}),
+		[duration, delay]
+	);
+
+	return (
+		<motion.div
+			initial={initial}
+			whileInView={{ x: 0, y: 0, opacity: 1 }}
+			transition={transition}
+			viewport={{ once: true, amount: 0.3 }}
+		>
+			{children}
+		</motion.div>
+	);
 }
