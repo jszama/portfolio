d92e73fe3397c4f523bcd6de39c0c1ec629b1c5d
jszama <jmszamotulski@gmail.com>
2025-08-07 17:29:00 +0100

    add modal content, refactor and optimise Experience sections

--- src/app/__components/experience/assets/experiences.tsx
+++ src/app/__components/experience/assets/experiences.tsx
@@ -1,123 +1,119 @@
-import { ReactNode } from 'react'
-import { Poland20Logo, OutlierLogo, PropecoLogo, Subsea7Logo } from './index'
-import { NextJsIcon, PythonIcon, ReactIcon, TypescriptIcon } from '../../TechStackIcons'
-import { StaticImageData } from 'next/image'
+import { StaticImageData } from "next/image";
+import { ReactNode } from "react";
+import { NextJsIcon, PythonIcon, ReactIcon, TypescriptIcon } from "../../TechStackIcons";
+import { OutlierLogo, Poland20Logo, PropecoLogo, Subsea7Logo } from "./index";
 
 export type Description = {
-    title: string
-    description: string[]
-}
+	title: string;
+	description: string[];
+};
 
 export type Experience = {
-    company: string
-    company_icon: StaticImageData
-    position: string
-    duration: string
-    description: Description
-    techStack?: string[]
-    readMore?: boolean
-    modalContent?: ReactNode
-}
+	company: string;
+	company_icon: StaticImageData;
+	position: string;
+	duration: string;
+	description: Description;
+	techStack?: string[];
+	readMore?: boolean;
+	modalContent?: ReactNode;
+};
 
 const ModalContent = {
-    PropEco: (
-        <div>
-            <h2>PropEco</h2>
-            <p>My first experience in a professional environment, where I learned about GIS and data management.</p>
-            <p>Worked with ArcGIS to manage and analyse geospatial data.</p>
-            <p>Automated data handling workflows with Python scripts, improving productivity by 40%.</p>
-        </div>
-    ),
-    Poland20: (
-        <div>
-            <h2>Poland 2.0</h2>
-            <p>My first software engineering role, I am in charge of maintaining and improving the company website.</p>
-            <p>Worked with ArcGIS to manage and analyse geospatial data.</p>
-            <p>Automated data handling workflows with Python scripts, improving productivity by 40%.</p>
-        </div>
-    )
+	PropEco: (
+		<div>
+			<h2>PropEco</h2>
+			<p>
+				My first experience in a professional environment, where I learned about GIS and
+				data management.
+			</p>
+			<p>Worked with ArcGIS to manage and analyse geospatial data.</p>
+			<p>
+				Automated data handling workflows with Python scripts, improving productivity by
+				40%.
+			</p>
+		</div>
+	),
+	Poland20: (
+		<div>
+			<h2>Poland 2.0</h2>
+			<p>
+				My first software engineering role, I am in charge of maintaining and improving the
+				company website.
+			</p>
+			<p>Worked with ArcGIS to manage and analyse geospatial data.</p>
+			<p>
+				Automated data handling workflows with Python scripts, improving productivity by
+				40%.
+			</p>
+		</div>
+	),
 };
 
 export const Experiences = [
-    {
-        company: 'PropEco',
-        company_icon: PropecoLogo,
-        position: 'Fullstack SWE Intern',
-        duration: 'Apr 2025 - Present',
-        description: {
-            title: 'My first experience in a professional environment, where I learned about GIS and data management.',
-            description: [
-                'Worked with ArcGIS to manage and analyse geospatial data.',
-                'Automated data handling workflows with Python scripts, improving productivity by 40%.',
-                'Processed and managed vessel route data using Excel, enhancing data accuracy and accessibility.',
-                'Collaborated with a team of engineers and geologists to support offshore projects.',
-                'Quickly adapted to new technologies, demonstrating effective problem-solving in a high-paced setting.'
-            ]
-        },
-        techStack: [
-            ReactIcon,
-            TypescriptIcon,
-            PythonIcon
-        ],
-        readMore: true,
-        modalContent: ModalContent.PropEco
-    },
-    {
-        company: 'Poland 2.0',
-        company_icon: Poland20Logo,
-        position: 'Digital Officer',
-        duration: 'Jan 2025 - Present',
-        description: {
-            title: 'My first software engineering role, I am in charge of maintaining and improving the company website.',
-            description: [
-                'Worked with ArcGIS to manage and analyse geospatial data.',
-                'Automated data handling workflows with Python scripts, improving productivity by 40%.',
-                'Processed and managed vessel route data using Excel, enhancing data accuracy and accessibility.',
-                'Collaborated with a team of engineers and geologists to support offshore projects.',
-                'Quickly adapted to new technologies, demonstrating effective problem-solving in a high-paced setting.'
-            ]
-        },
-        techStack: [
-            NextJsIcon,
-            ReactIcon,
-            TypescriptIcon,
-        ],
-        readMore: true,
-        modalContent: ModalContent.Poland20
-    },
-    {
-        company: 'Outlier',
-        company_icon: OutlierLogo,
-        position: 'AI Content Reviewer',
-        duration: 'Nov 2024 - Present',
-        description: {
-            title: 'My first experience in a professional environment, where I learned about GIS and data management.',
-            description: [
-                'Worked with ArcGIS to manage and analyse geospatial data.',
-                'Automated data handling workflows with Python scripts, improving productivity by 40%.',
-                'Processed and managed vessel route data using Excel, enhancing data accuracy and accessibility.',
-                'Collaborated with a team of engineers and geologists to support offshore projects.',
-                'Quickly adapted to new technologies, demonstrating effective problem-solving in a high-paced setting.'
-            ]
-        },
-    },
-    {
-        company: 'Subsea7',
-        company_icon: Subsea7Logo,
-        position: 'GIS Technician Intern',
-        duration: 'Nov 2022 - Mar 2023',
-        description: {
-            title: 'My first experience in a professional environment, where I learned about GIS and data management.',
-            description: [
-                'Worked with ArcGIS to manage and analyse geospatial data.',
-                'Automated data handling workflows with Python scripts, improving productivity by 40%.',
-                'Processed and managed vessel route data using Excel, enhancing data accuracy and accessibility.',
-                'Collaborated with a team of engineers and geologists to support offshore projects.',
-                'Quickly adapted to new technologies, demonstrating effective problem-solving in a high-paced setting.'
-            ]
-        },
-        techStack: [
-            PythonIcon,
-        ]
-    },
+	{
+		company: "PropEco",
+		company_icon: PropecoLogo,
+		position: "Fullstack Engineer",
+		duration: "Apr 2025 - Present",
+		description: {
+			title: "My first professional Software Engineering position, at a start-up.",
+			description: [
+				"Deployed a self-hosted Large Language Model (LLM) on AWS, reducing third-party API costs by ~50% and improving data privacy and control.",
+				"Built a report ordering and auto PDF generation system using Stripe, Puppeteer, and API key authentication, enabling seamless paid data access.",
+				"Developed embeddable widgets for client websites to display live company API data, enhancing customer engagement and platform integration.",
+			],
+		},
+		techStack: [ReactIcon, TypescriptIcon, PythonIcon],
+		readMore: true,
+		modalContent: ModalContent.PropEco,
+	},
+	{
+		company: "Poland 2.0",
+		company_icon: Poland20Logo,
+		position: "Frontend Engineer",
+		duration: "Jan 2025 - Present",
+		description: {
+			title: "My first Software Engineering role, I am in charge of maintaining and improving the company website.",
+			description: [
+				"Integrated internationalisation (i18n) support using Lingui, enabling bilingual functionality in English and Polish.",
+				"Designed and developed a dynamic blog system with markdown support, allowing the marketing team to publish and manage content independently.",
+				"Optimised performance by implementing lazy loading and next-gen image formats (WebP), reducing image load times by ~45% and improving Lighthouse score by 5+ points.",
+			],
+		},
+		techStack: [NextJsIcon, ReactIcon, TypescriptIcon],
+		readMore: true,
+		modalContent: ModalContent.Poland20,
+	},
+	{
+		company: "Outlier",
+		company_icon: OutlierLogo,
+		position: "AI Content Reviewer",
+		duration: "Nov 2024 - Present",
+		description: {
+			title: "Freelancing remote job, in charge of reviewing AI-generated content.",
+			description: [
+				"Reviewed and provided feedback on AI-generated content for accuracy and relevance.",
+				"Greatly improved my attention to detail and understanding of AI content generation.",
+				"Developed precise prompt engineering skills to enhance AI output quality.",
+			],
+		},
+	},
+	{
+		company: "Subsea7",
+		company_icon: Subsea7Logo,
+		position: "GIS Technician Intern",
+		duration: "Nov 2022 - Mar 2023",
+		description: {
+			title: "My first experience in a professional environment, where I learned about GIS and data management.",
+			description: [
+				"Worked with ArcGIS to manage and analyse geospatial data.",
+				"Automated data handling workflows with Python scripts, improving productivity by 40%.",
+				"Processed and managed vessel route data using Excel, enhancing data accuracy and accessibility.",
+				"Collaborated with a team of engineers and geologists to support offshore projects.",
+				"Quickly adapted to new technologies, demonstrating effective problem-solving in a high-paced setting.",
+			],
+		},
+		techStack: [PythonIcon],
+	},
 ] as Experience[];
--- src/app/__components/experience/index.tsx
+++ src/app/__components/experience/index.tsx
@@ -1,27 +1,42 @@
 "use client";
 
-import React from 'react'
-import ExperienceCard from './ExperienceCard'
-import { Experiences } from './assets/experiences'
-import { useExpandedItem } from './useExpandedItem'
+import { useState } from "react";
+import { useScrollOverflow } from "../hooks";
+import ScrollArrows from "../ScrollArrows";
+import ExperienceCard from "./assets/ExperienceCard";
+import { Experiences } from "./assets/experiences";
+import { useExpandedItem } from "./assets/useExpandedItem";
 
 export default function Experience() {
-  const { expandedItem, toggleItem } = useExpandedItem();
+	const { expandedItem, toggleItem } = useExpandedItem();
+	const [showArrows, setShowArrows] = useState(false);
+	const { sliderRef, overflowing } = useScrollOverflow();
 
-  return (
-    <section className='flex flex-col px-4 py-12 sm:mx-24 sm:py-0 sm:pb-12 sm:px-0' id='experience'>
-        <h2 className='text-3xl text-[var(--primary-text)] font-bold'>Experience</h2>
-        <h3 className='text-md text-[var(--secondary-text)] mb-4'>My professional journey so far...</h3>
-        <div className="flex flex-row gap-x-6 px-2 py-4 overflow-x-auto snap-x scrollbar-thin scrollbar-thumb-[var(--hover-magenta)] scrollbar-track-[var(--secondary-dark-background)]">
-          {Experiences.map((experience) => (
-            <ExperienceCard
-              key={experience.company}
-              {...experience}
-              isExpanded={expandedItem === experience.company}
-              onToggle={() => toggleItem(experience.company)}
-            />
-          ))}
-        </div>
-    </section>
-  )
+	return (
+		<section
+			className="flex flex-col px-4 py-12 sm:mx-24 sm:py-0 sm:pb-12 sm:px-0 relative"
+			id="experience"
+			onMouseEnter={() => setShowArrows(true)}
+			onMouseLeave={() => setShowArrows(false)}
+		>
+			<h2 className="text-3xl text-[var(--primary-text)] font-bold">Experience</h2>
+			<h3 className="text-md text-[var(--secondary-text)] mb-4">
+				My professional journey so far...
+			</h3>
+			<div
+				className="flex space-x-6 p-2 overflow-x-scroll snap-x snap-mandatory scrollbar-thumb-[var(--hover-magenta)] scrollbar-track-[var(--secondary-dark-background)] scrollbar-hide overscroll-x-contain"
+				ref={sliderRef}
+			>
+				{Experiences.map((experience) => (
+					<ExperienceCard
+						key={experience.company}
+						{...experience}
+						isExpanded={expandedItem === experience.company}
+						onToggle={() => toggleItem(experience.company)}
+					/>
+				))}
+			</div>
+			{overflowing && showArrows && <ScrollArrows sliderRef={sliderRef} />}
+		</section>
+	);
 }
