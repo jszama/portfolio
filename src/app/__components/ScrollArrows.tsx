89d19429840c51caa4290cec101114490b014334
jszama <jmszamotulski@gmail.com>
2025-08-22 17:40:06 +0100

    update dependencies and remove lucide-react

--- src/app/__components/ScrollArrows.tsx
+++ src/app/__components/ScrollArrows.tsx
@@ -1,4 +1,3 @@
-import { ChevronLeft, ChevronRight } from "lucide-react";
 import { type RefObject } from "react";
 import { useThrottle } from "./hooks";
 
@@ -24,13 +23,37 @@ export function ScrollArrows({ sliderRef }: { sliderRef: RefObject<HTMLDivElemen
 				className="cursor-pointer absolute top-1/2 -translate-y-1/2 left-5 bg-black/50 flex items-center justify-center rounded-full"
 				onClick={scrollLeft}
 			>
-				<ChevronLeft size={32} />
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path d="m15 18-6-6 6-6" />
+				</svg>
 			</button>
 			<button
 				className="cursor-pointer absolute top-1/2 -translate-y-1/2 right-5 bg-black/50 flex items-center justify-center rounded-full"
 				onClick={scrollRight}
 			>
-				<ChevronRight size={32} />
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<path d="m9 18 6-6-6-6" />
+				</svg>
 			</button>
 		</div>
 	);
