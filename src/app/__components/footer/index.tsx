9eec8fb48f071d08503a02f65281ae7ed86847b9
jszama <jmszamotulski@gmail.com>
2025-09-04 18:43:25 +0100

    add github repo link to footer

--- src/app/__components/footer/index.tsx
+++ src/app/__components/footer/index.tsx
@@ -4,7 +4,17 @@ export default function Footer() {
 			className="flex flex-col justify-center h-full w-full gap-4 pt-4 sm:pt-6 pb-4 bg-[var(--primary-dark-background)] border-t-2 border-[var(--primary-magenta)]"
 			id="footer"
 		>
-			<p className="text-[var(--accent-text)] text-center">Designed and built by me :)</p>
+			<p className="text-[var(--accent-text)] text-center">
+				Designed and built by me :){" "}
+				<a
+					href="https://github.com/jszama/portfolio"
+					target="_blank"
+					rel="noopener noreferrer"
+					className="text-[var(--hover-magenta)] underline font-semibold"
+				>
+					Repo here
+				</a>
+			</p>
 		</footer>
 	);
 }
