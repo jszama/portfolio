3a4a91209aeaef03a56d6eb07e3255cb35bc5ba8
jszama <jmszamotulski@gmail.com>
2025-12-12 00:20:16 +0000

    add BCG dagmara kowalska testimonial

--- src/app/__components/testimonials/assets/index.tsx
+++ src/app/__components/testimonials/assets/index.tsx
@@ -1,8 +1,18 @@
 import { type Testimonial } from "./TestimonialCard";
 import Chris_Hardman from "./chris_hardman.webp";
+import Dagmara_Kowalska from "./dagmara_kowalska.webp";
 import Michael_Ajayi from "./michael_ajayi.webp";
 
 export const testimonials: Testimonial[] = [
+	{
+		name: "Dagmara Kowalska",
+		role: "Talent Acquisition Snr. Manager",
+		company: "Boston Consulting Group",
+		testimonial:
+			"Jakub demonstrated professionalism, organizational skills, and dedication, making a significant contribution to the success of the 2025 Poland 2.0 summit. We recommend Jakub Szamotulski to any organization seeking a capable, motivated, and trustworthy individual.",
+		image: Dagmara_Kowalska,
+		link: "https://www.linkedin.com/in/dagmara-kowalska-gumna/",
+	},
 	{
 		name: "Chris Hardman",
 		role: "Founder & CEO",
