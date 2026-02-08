ba239daf268e816cc485596d845796d5bcb798b9
jszama <jmszamotulski@gmail.com>
2026-02-08 22:14:34 +0000

    add jpmorgan logo

--- src/app/__components/experience/assets/index.ts
+++ src/app/__components/experience/assets/index.ts
@@ -1,7 +1,8 @@
+import JPMorganLogo from "./jpmc.webp";
 import LoopsioLogo from "./loopsio.webp";
 import OutlierLogo from "./outlier.webp";
 import Poland20Logo from "./poland20.webp";
 import PropecoLogo from "./propeco.webp";
 import Subsea7Logo from "./subsea7.webp";
 
-export { LoopsioLogo, OutlierLogo, Poland20Logo, PropecoLogo, Subsea7Logo };
+export { JPMorganLogo, LoopsioLogo, OutlierLogo, Poland20Logo, PropecoLogo, Subsea7Logo };
